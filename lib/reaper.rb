128547081e806bf2408a2520e0dad7eae1c83dc8
phantomfreeJr <vdavis@siu.edu>
2017-10-02 22:11:28 -0500

    add exploit folder to tools

--- lib/reaper.rb
+++ lib/reaper.rb
@@ -11,13 +11,13 @@ module Reaper
   def self.get_first_arg
     arg = ARGV[0]
     if arg == GENERATE
-      require 'reaper/tools/generate'
+      require 'reaper/tools/exploit/generate'
     elsif arg == PATTERN_CREATE
-      require 'reaper/tools/pattern_create'
+      require 'reaper/tools/exploit/pattern_create'
     elsif arg == PATTERN_OFFSET
-      require 'reaper/tools/pattern_offset'
+      require 'reaper/tools/exploit/pattern_offset'
     elsif arg == HELP
-      Reaper.help_message
+      help_message
     else
       puts 'No options selected, try: "reaper help" for usage'
     end
