56cef8f3ddd9cf90dfd63d3346bb90dd7bb9e0b4
EasyIP2023 <vdavis@siu.edu>
2018-01-19 12:20:20 -0600

    change way ARGV is handled

--- lib/darkshadow/reap/command_names.rb
+++ lib/darkshadow/reap/command_names.rb
@@ -5,3 +5,13 @@ GENERATE       = "gen"
 PATTERN_CREATE = "pate"
 PATTERN_OFFSET = "poset"
 PORT_SEARCH    = "scan"
+
+def help_message
+  puts "Usage: darkshadow [options]\nExample: darkshadow pate -l 600"
+  puts "\nOptions:
+    #{GENERATE} \t Exploit Code Generation
+    #{PATTERN_CREATE} \t For Pattern Creating
+    #{PATTERN_OFFSET} \t For Pattern Offsetting
+    #{PORT_SEARCH} \t For Port Searching
+    #{HELP} \t for this message here"
+end
