dd6bcf08ede833d135bf3634d5bb818c89f611b7
EasyIP2023 <vdavis@siu.edu>
2018-01-21 16:00:25 -0600

    add banners

--- lib/darkshadow/reap/command_names.rb
+++ lib/darkshadow/reap/command_names.rb
@@ -1,8 +1,12 @@
+require 'darkshadow/reap/banners'
+require 'darkshadow/version'
 require 'colorize'
 
 # Set command names
 HELP           = "help"
 HELP_TAC       = "-h"
+VERS           = "version"
+VERS_TAC       = "-v"
 GENERATE       = "gen"
 PATTERN_CREATE = "pate"
 PATTERN_OFFSET = "poset"
@@ -11,6 +15,8 @@ EGG_HUNTER     = "egghunt"
 DARK_SHADOW    = "darkshadow".colorize(:light_red)
 
 def help_message
+  puts Banners.rand_banner
+  puts
   puts "Usage: #{DARK_SHADOW} [options]\nExample: #{DARK_SHADOW} #{PATTERN_CREATE.colorize(:light_yellow)} -l 600"
   puts "\nOptions:\n
     #{"Exploitation".colorize(:light_red)}
@@ -21,5 +27,10 @@ def help_message
     #{"Scanning".colorize(:light_red)}
     #{PORT_SEARCH.colorize(:light_green)} \t For Port Searching\n
     #{"Help Message".colorize(:light_red)}
-    #{HELP.colorize(:light_green)} \t for this message here"
+    #{HELP.colorize(:light_green)} \t for this message here
+    #{VERS.colorize(:light_green)} \t for version number\n"
+end
+
+def display_version
+  puts "darkshadow #{DarkShadow::VERSION}"
 end
