80389b3f0152bc6ea674d8b9920fc73a7a4268c1
EasyIP2023 <vdavis@siu.edu>
2018-02-03 13:08:22 -0600

    update version

--- ext/darkshadow.c
+++ ext/darkshadow.c
@@ -1,12 +1,7 @@
 #include "darkshadow.h"
 
-void rb_darkshadow_print(VALUE self, char *s) {
-  printf("%s\n", s);
-}
-
 void Init_darkshadow(void) {
   DEBUG_PRINT("Init_darkshadow");
   reap = rb_define_module("Reap");
-  rb_define_module_function(reap, "d_print", rb_darkshadow_print, 1);
   Init_sniffer();
 }
--- lib/darkshadow/tools/sniffer/packet_sniffer.rb
+++ lib/darkshadow/tools/sniffer/packet_sniffer.rb
@@ -1,7 +1,4 @@
-require 'darkshadow/reap/command_names'
-
 require 'darkshadow.so'
-include Reap
 
 puts "The Output will be written in a log file called dsniff_log.txt"
-PSniff.start_sniffing
+REAP::PSniff.start_sniffing
